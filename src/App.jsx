c1e6d696e2d48dcd570c5b74bfc49f06e6fe6148
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-15 22:08:27 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido a JG Assistants</h1>
+      <p>¡Ya estamos en producción!</p>
+    </div>
+  );
+}
+
+export default App;
